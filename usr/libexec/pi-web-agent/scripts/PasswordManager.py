2dc00c290a13cca9bea4c461653a9b7426d2231e
vaslabs <vaslabs@yahoo.gr>
2013-12-09 16:00:26 +0000

    Cosmetics

--- usr/libexec/pi-web-agent/scripts/PasswordManager.py
+++ usr/libexec/pi-web-agent/scripts/PasswordManager.py
@@ -13,13 +13,17 @@ import subprocess
 from live_info import execute
 
 def getView():
-    iwpasswd = InputWidget('password', 'passwd', '', 'Current password: ',wClass='form-control password',attribs='placeholder="Current Password"')
-    iwpasswd_new1=InputWidget('password', 'passwd_new1', '', 'Set new Password: ',wClass='form-control password',attribs='placeholder="New Password"')
-    iwpasswd_new2=InputWidget('password', 'passwd_new2', '', 'Retype password: ',wClass='form-control password',attribs='placeholder="Retype Password"')
+    iwpasswd = InputWidget('password', 'passwd', '', 'Current password: ',\
+    wClass='form-control password',attribs='placeholder="Current Password"')
+    iwpasswd_new1=InputWidget('password', 'passwd_new1', '', 'Set new Password: ',\
+    wClass='form-control password',attribs='placeholder="New Password"')
+    iwpasswd_new2=InputWidget('password', 'passwd_new2', '', 'Retype password: ',\
+    wClass='form-control password',attribs='placeholder="Retype Password"')
     iw_submit=InputWidget('submit', '', 'Change password', '',wClass='btn btn-primary')
     iwg = InputWidgetGroup()
     iwg.widgets=[iwpasswd, iwpasswd_new1, iwpasswd_new2, iw_submit] 
-    return fieldset('/cgi-bin/toolkit/change_password.py', 'POST', 'password_form', iwg, createLegend("Change password"))
+    return fieldset('/cgi-bin/toolkit/change_password.py', 'POST', \
+    'password_form', iwg, createLegend("Change password"))
 
 
 def getSuccessView():
