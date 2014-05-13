f05c009c6970a87e5407c67ab24d8c9c3925397c
angelos <a2gelosgeo@gmail.com>
2014-05-13 23:57:59 +0000

    removed unused/experimental code

--- usr/libexec/pi-web-agent/css/autocomplete.js
+++ usr/libexec/pi-web-agent/css/autocomplete.js
@@ -14,7 +14,7 @@ $(function() {
       } 
   });
 
-
+  //added regex to much the start of each package name
   $( "#autocomplete" ).autocomplete({
   source: function( request, response ) {
       var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
@@ -27,7 +27,9 @@ $(function() {
 });
 
 var textContentProp = "innerText" in document.body ? "innerText" : "textContent";
-function filter ( _input_id, _table_id, cellNr){
+// filters the table depending on the text in the search box after a keystroke
+// or when a new row is added in the table
+function filter ( _input_id, _table_id, cellNr ){
   if( document.getElementById( _input_id ) != null ){  
     var inputValue = document.getElementById( _input_id ).value.toLowerCase();
     var table = document.getElementById(_table_id);
@@ -41,44 +43,3 @@ function filter ( _input_id, _table_id, cellNr){
 	  }
 	}
 }
-
-
-/*
-var textContentProp = "innerText" in document.body ? "innerText" : "textContent";
-function updateView(){
-  var table = document.getElementById("packages-table-id");
-  var ui = document.getElementById("ui-id-1");
-  var li = ui.getElementsByTagName("li");
-
-  var suggestionList = [];
-
-  for (var i=0; i < li.length; i++) {
-    //alert( li[i].innerHTML );
-    alert( li[i][textContentProp] );
-    suggestionList.push( li[i][textContentProp] );
-  }
-
-  $.ajax({
-      url: '/cgi-bin/toolkit/package_recommendations.py?action=updatePackageListView',
-      type: 'get',
-      dataType: 'json',
-      async: true,
-      success: function(data) {
-        for( var key in data ){
-          alert( key );
-        }
-
-      //iterate in data and get rid of the missing packages depending on what's in the suggestion list.
- //     buildHtmlTable( response, firstTime );
-      } 
-  });
-
-}
-
-
-function getText()
-{
-    return document.getElementById('menu_selected')[textContentProp];
-}
-*/
-
