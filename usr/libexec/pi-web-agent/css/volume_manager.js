7ff7d6e38ade7a4fe9398e38575be5692bcc6386
Argyris Zardilis <azardilis@gmail.com>
2014-07-11 14:24:11 +0100

    added code for slider things

--- usr/libexec/pi-web-agent/css/volume_manager.js
+++ usr/libexec/pi-web-agent/css/volume_manager.js
@@ -1,10 +1,20 @@
 
-
-
 function create_controls() {
 
+    $( "#slider-vertical" ).slider({
+      orientation: "vertical",
+      range: "min",
+      min: 0,
+      max: 100,
+      value: 60,
+      slide: function( event, ui ) {
+        $( "#amount" ).val( ui.value );
+      }
+    });
+    $( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
+}
 
- }
-
-
-create_controls()
+$(function() {
+    create_controls()
+    
+});
