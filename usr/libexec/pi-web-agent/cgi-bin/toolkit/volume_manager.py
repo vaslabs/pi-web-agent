adc464030608cd3a7c3b6e34c9c5ba11008864d5
Argyris Zardilis <azardilis@gmail.com>
2014-07-11 14:23:41 +0100

    added html for slider labels etc

--- usr/libexec/pi-web-agent/cgi-bin/toolkit/volume_manager.py
+++ usr/libexec/pi-web-agent/cgi-bin/toolkit/volume_manager.py
@@ -15,10 +15,31 @@ from volume_controller import get_volume
 
 
 def get_view():
-    vol = get_volume()
-    content = "Current volume is: " + vol
+    content =  '''
+    <link rel="stylesheet" href="/css/jquery-ui.css">
+    <script src="/css/jquery-ui.js"></script>
+    <script src="/css/volume_manager.js" type="text/javascript"></script>
+    
+    <script src="/css/appDefinitions.js" type="text/javascript"></script>
+    <link href="/css/openDialog.css" type="text/css" rel="stylesheet" />
+    
+    <p class="ui-state-default ui-corner-all ui-helper-clearfix" style="padding:4px;">
+    <span class="ui-icon ui-icon-volume-on" style="float:left; margin:-2px 5px 0 0;"></span>
+    Master
+    </p>
 
-    return content
+    <p>
+    <label for="amount">Volume:</label>
+    <input type="text" id="amount" readonly style="border:0; color:#f6931f; font-weight:bold;">
+    </p>
+ 
+    <div id="slider-vertical" style="height:200px;"></div>
+
+    Curent volume is: {vol}
+    
+    '''
+    
+    return content.format(vol=get_volume())
     
 def main():
     fs = cgi.FieldStorage()
