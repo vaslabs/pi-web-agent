3f21b0f4716129154b289627967800f408d807ba
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-07-12 19:28:12 +0000

    Dependable

--- usr/libexec/pi-web-agent/cgi-bin/toolkit/camera_manager.py
+++ usr/libexec/pi-web-agent/cgi-bin/toolkit/camera_manager.py
@@ -17,15 +17,20 @@ import cgitb
 from subprocess import Popen, PIPE
 cgitb.enable()
 from framework import output, view, config
+from DependableExtension import DependableExtension
 
-def main():
+EXTENSION_ID='Pi Camera Controller'
 
-    form = cgi.FieldStorage()
+class CameraManager(DependableExtension):
+
+    def __init__(self):
+        DependableExtension.__init__(self, EXTENSION_ID)
+        
+    def getNormalView(self):
+        pictures, returncode = execute("ls /usr/share/pi-web-agent/camera-media/*.jpg")
+        linearray = pictures.split('\n')
     
-    pictures, returncode = execute("ls /usr/share/pi-web-agent/camera-media/*.jpg")
-    linearray = pictures.split('\n')
-    
-    html = '''<div id="camera_toolbar">
+        html = '''<div id="camera_toolbar">
                   <div class="btn-group btn-group-justified">
                       <a href='javascript:navigate("/cgi-bin/toolkit/camera.py?type=js")' class="btn btn-default">Live stream</a>
                       <a href='javascript:camera_utils("snapshot")' class="btn btn-default">Snapshot</a>
@@ -34,16 +39,28 @@ def main():
       
                  </div></div><br>
           '''
-    html += '''<div id="gallery_thumbnails">'''
+        html += '''<div id="gallery_thumbnails">'''
     
-    for thisline in linearray:
-        justname = thisline.split('/')[-1]
-        if len(justname) <= 0:
-            continue
-        html += '<a href="/cgi-bin/toolkit/image_manager.py?image='+justname +'" rel="thumbnail"><img style="padding:4px; border:2px solid #021a40;" src="/cgi-bin/toolkit/image_manager.py?image='+justname.split('.')[0]+'.png" style="width: 64px; height: 64px" /></a>'
-    html += '</div><br>'
+        for thisline in linearray:
+            justname = thisline.split('/')[-1]
+            if len(justname) <= 0:
+                continue
+            html += '<a href="/cgi-bin/toolkit/image_manager.py?image='+justname +'" rel="thumbnail"><img style="padding:4px; border:2px solid #021a40;" src="/cgi-bin/toolkit/image_manager.py?image='+justname.split('.')[0]+'.png" style="width: 64px; height: 64px" /></a>'
+        
+        html += '</div><br>'
+        return html
+    
+    def generateView(self):
+        if (not self.check_status()):
+            return self._generateMissingDependenciesView()
+        return self.getNormalView()
 
-    view.setContent('Pi Camera Controller', html)
+def main():
+
+    form = cgi.FieldStorage()
+    
+    cameraMgr = CameraManager()
+    view.setContent('Pi Camera Controller', cameraMgr.generateView())
     output(view, cgi.FieldStorage())
     
 if __name__ == '__main__':
