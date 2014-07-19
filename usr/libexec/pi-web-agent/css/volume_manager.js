440b589ea9808c8abc1607fb9f514eee07032c7f
Argyris Zardilis <azardilis@gmail.com>
2014-07-19 21:55:01 +0100

    added api calls from interface slider events

--- usr/libexec/pi-web-agent/css/volume_manager.js
+++ usr/libexec/pi-web-agent/css/volume_manager.js
@@ -1,20 +1,38 @@
 
-function create_controls() {
+function init() {
+    url = '/cgi-bin/toolkit/volume.py';
+    getJSONResponse(url, create_controls);
+}
 
+function create_controls(data) {
     $( "#slider-vertical" ).slider({
       orientation: "vertical",
       range: "min",
       min: 0,
       max: 100,
-      value: 60,
+      value: data,
       slide: function( event, ui ) {
         $( "#amount" ).val( ui.value );
+	url = '/cgi-bin/toolkit/volume.py?update=' + ui.value;
+	getJSONResponse(url, handle_vol_update)
+	
       }
     });
     $( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
 }
 
+function handle_vol_update(data) {
+    // data: response from volume update call
+    // if error report it
+
+    return 0;
+}
+
+    
+
+
+
 $(function() {
-    create_controls()
+    init()
     
 });
