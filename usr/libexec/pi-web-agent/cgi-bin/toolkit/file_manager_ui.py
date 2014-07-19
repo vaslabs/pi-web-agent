b01aaa0848276939ee0707456ece81639d2ec960
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-07-19 18:50:06 +0000

    Refactored with template

--- usr/libexec/pi-web-agent/cgi-bin/toolkit/file_manager_ui.py
+++ usr/libexec/pi-web-agent/cgi-bin/toolkit/file_manager_ui.py
@@ -4,36 +4,12 @@ import os, sys
 if 'MY_HOME' not in os.environ:
     os.environ['MY_HOME']='/usr/libexec/pi-web-agent'
 sys.path.append(os.environ['MY_HOME'] + '/etc/config')
-from framework import view, output
+from framework import view, output, get_template
 
 def main():
-    content = '''
-        <ul id="bpath" class='breadcrumb' style="margin-bottom: 5px;">
-        </ul>
-        <table id="file-manager-table" class="table table-striped table-hover ">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Type</th>
-                    <th>Modified</th>
-                    <th>Owner</th>
-                    <th>Group</th>
-                    <th>Size</th>
-                </tr>
-            </thead>
-        </table>
-        <div id="openDialog" title="Open with.">
-  				<p>Choose a web-agent application to open your file with</p>
-  				<ul>
-  				</ul>
-		</div>
-				<link rel="stylesheet" href="/css/jquery-ui.css">
-		<script src="/css/jquery-ui.js"></script>
-		<script src="/css/file_manager.js" type="text/javascript"></script>
-        
-		 <script src="/css/appDefinitions.js" type="text/javascript"></script>
-        <link href="/css/openDialog.css" type="text/css" rel="stylesheet" />
-    '''
+    tFile = open(get_template('file_manager_controller'))
+    content = tFile.read()
+    tFile.close()
     view.setContent('File manager', content)
     output(view, cgi.FieldStorage())
 if __name__ == "__main__":
