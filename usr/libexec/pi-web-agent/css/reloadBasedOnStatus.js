9a192d1dcd1cea5a74395ad2bc8bab1390e994c6
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-07-20 15:12:45 +0000

    Fixed careless written url

--- usr/libexec/pi-web-agent/css/reloadBasedOnStatus.js
+++ usr/libexec/pi-web-agent/css/reloadBasedOnStatus.js
@@ -7,6 +7,6 @@ function checkAptBusy(response){
         setTimeout(function() {getResponse('/cgi-bin/toolkit/live_info.py?cmd=apt',
                     checkAptBusy)}, 5000); 
     else
-        navigate('https://raspberrypi:8003/cgi-bin/toolkit/package_recommendations.py?type=js');
+        navigate('/cgi-bin/toolkit/package_recommendations.py?type=js');
 }
 
