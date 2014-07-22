2e4fbf9df5018f9751c66681035b1834ff1afd02
Argyris Zardilis <azardilis@gmail.com>
2014-07-23 00:14:51 +0100

    added function to get mixers and changed api to include arg for mixer

--- usr/libexec/pi-web-agent/cgi-bin/toolkit/volume_api.py
+++ usr/libexec/pi-web-agent/cgi-bin/toolkit/volume_api.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 import json
 import os, sys
-from live_info import execute
 import cgi, cgitb
 cgitb.enable()
 if 'MY_HOME' not in os.environ:
@@ -10,44 +9,60 @@ sys.path.append(os.environ['MY_HOME'] + '/etc/config')
 
 from HTMLPageGenerator import composeJS
 from framework import view, output
+from functools import partial
+from live_info import execute
 import re
 
-def get_volume():
-    # Returns current volume of Master    
-    out, exit_code = execute("sudo amixer sget PCM")
+def get_volume(args):
+    # Returns current volume of args['mixer']
+    command = "sudo amixer sget {mixer}"
+    out, exit_code = execute(command.format(mixer=args['mixer']))
     m = re.search("[0-9]+%", out)
     vol = m.group(0)[:-1]
     
-    return int(vol)
+    return json.dumps(int(vol))
 
-def set_volume(n_vol):
-    command = "sudo amixer sset PCM {vol}\%"
-    out, exit_code = execute(command.format(vol=n_vol))
+def set_volume(args):
+    # Sets volume of mixer specified in args
+    command = "sudo amixer sset {mixer} {vol}\%"
+    out, exit_code = execute(command.format(mixer=args['mixer'],
+                                            vol=args['val']))
 
-    return n_vol
+    return json.dumps(args)
 
+def get_mixers(args):
+    out, exit_code = execute("sudo amixer scontrols")
+    mixers = re.findall(r"'.+'", out)
+    mixers = list(m[1:-1] for m in mixers)
+
+    return json.dumps(mixers)
+
+def error():
+    # Do something if operation code not found!
+    pass
+    
+def op_dispatch(form):
+    op = form.getfirst("op")
+    args = dict((k, form[k].value) for k in form.keys() if not k=="op")
+
+    op_dict = {
+        'get_vol'    : partial(get_volume, args=args),
+        'update_vol' : partial(set_volume, args=args),
+        'mixers'     : partial(get_mixers, args=args)
+    }
+
+    
+    op_func = op_dict.get(op, error)
+    return op_func()
+    
 def main():
     # main entry point for the volume controller api
     # do a dispatch on the url command and call
     # the corresponding function
     #
-    # If no value is given returns current volume
-    # otherwise sets volume to the update value in
-    # the form from GET
-    #
-    # Need to also check for not well formed URLs
+    # TODO: fail gracefully
     form = cgi.FieldStorage()
-
-    try:
-        new_vol = form['update'].value
-        n_vol = set_volume(int(new_vol))
-        composeJS(json.dumps(n_vol))
-    except KeyError:
-        current_vol = get_volume()
-        composeJS(json.dumps(current_vol))
-
-if __name__ == "__main__":
-    main()
+    op_dispatch(form)
 
 
 
