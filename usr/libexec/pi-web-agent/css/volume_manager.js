a963bb053f4d603bc690751a8ef5435e44647354
Argyris Zardilis <azardilis@gmail.com>
2014-07-23 15:27:57 +0100

    uncommented lines that didn't work. works now

--- usr/libexec/pi-web-agent/css/volume_manager.js
+++ usr/libexec/pi-web-agent/css/volume_manager.js
@@ -10,24 +10,23 @@ function create_controls(data) {
       range: "min",
       min: 0,
       max: 100,
-      value: 5,
+      value: data,
       stop: function( event, ui ) {
         $( "#amount" ).val( ui.value );
-	//url = '/cgi-bin/toolkit/volume_api.py?op=update_vol&mixer=PCM&val=' + ui.value;
-	//getJSONResponse(url, handle_vol_update)
+	url = '/cgi-bin/toolkit/volume_api.py?op=update_vol&mixer=PCM&val=' + ui.value;
+	getJSONResponse(url, handle_vol_update)
       }
     });
     $( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
 }
 
-//function handle_vol_update(data) {
+function handle_vol_update(data) {
     // data: response from volume update call
     // if error report it
 
-//    return 0;
-//}
+    return 0;
+}
 
 $(function() {
-    create_controls()
-    
+    init()
 });
