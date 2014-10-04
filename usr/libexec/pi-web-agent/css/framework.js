a8b5193f28744a05170450d58e474f6c43339568
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-10-04 18:43:46 +0000

    First stage of knockout and C migration finished

--- usr/libexec/pi-web-agent/css/framework.js
+++ usr/libexec/pi-web-agent/css/framework.js
@@ -8,16 +8,35 @@ function PiWebAgent () {
 
 function View() {
     self = this;
-    self.actions = null;
-    self.extensions = null;
+    self.actions = [];
+    self.extensions = [
+                      ];
+    self.extensionsOverflow = [];
+    self.extensionLimit = 5;
     fetchViewData();
     
     
     function fetchViewData() {
-        data = getJSONResponse('/cgi-bin/chrome/view.pwa');
-        self.extensions = data[0]["pi-web-agent"].system.actions;
-        self.actions = data[1]["pi-web-agent"].actions;
-    }
+        data = getJSONResponse('/cgi-bin/chrome/view.pwa', null);
+        var extensionsCounter = 0;
+        $.each(data[0]["pi-web-agent"].system.actions, function (key, action) {
+            if (action.title.split(' ').length >= 3)
+                self.extensionsOverflow.push(action);
+            else if (extensionsCounter < self.extensionLimit) {
+                self.extensions.push(action);
+                extensionsCounter++; 
+            }
+            else
+                self.extensionsOverflow.push(action);
+                    });
+        
+        $.each(data[1]["pi-web-agent"].actions, function (key, action) {
+            self.actions.push(action);
+        });
+    };
     
 }
-ko.applyBindings(new View());
+$(function() {
+    var view = new View();
+    ko.applyBindings(view);
+});
