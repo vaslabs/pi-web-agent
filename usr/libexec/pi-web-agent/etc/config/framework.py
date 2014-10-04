4dc2d9fb9617af06c498c819bb2cfa1aa56fc7c6
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-10-04 18:31:55 +0000

    maintain python compatibility

---
+++ usr/libexec/pi-web-agent/etc/config/framework.py
@@ -0,0 +1,27 @@
+import sys
+import os
+if 'MY_HOME' not in os.environ:
+    os.environ['MY_HOME']='/usr/libexec/pi-web-agent'
+sys.path.append(os.environ['MY_HOME']+'/etc/config')
+sys.path.append(os.environ['MY_HOME']+'/cgi-bin/chrome')
+sys.path.append(os.environ['MY_HOME']+'/objects')
+from pi_web_agent import Configuration
+from view import View
+
+config=Configuration()
+view = View(config.system.actions, config.system.cmdactions)
+
+def get_template(template):
+    return os.environ['MY_HOME']+'/templates/' + template + '.htm'
+
+def output(view, form):
+    if "type" in form and form["type"].value == "js":
+        view.js_output()
+    else:
+        view.output()
+        
+def requestDefinition(extensionID):
+    return config.system.actions[extensionID]
+        
+def getDependencies(extensionID):
+    return config.system.actions[extensionID]['dependencies']
---
+++ usr/libexec/pi-web-agent/etc/config/pi_web_agent.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import os
+import json
+
+CONFIG_FILE=os.environ['MY_HOME']+"/etc/config/config.cfg"
+CONFIG_PATH=os.environ['MY_HOME']+"/etc/config"
+VERSION="0.3"
+class Configuration(object):
+    
+    def __init__(self):
+        json_file = open(CONFIG_FILE)
+        self.config=json.load(json_file)
+        json_file.close()
+        system=self.config['pi-web-agent']['system']    
+        actions = system['actions']
+        self.system=System()
+        self.system.actions = actions
+        try:
+            extensionFile=system['extension']
+            json_file = open(CONFIG_PATH+'/'+extensionFile)
+            extension = json.load(json_file)
+            json_file.close()
+            cmd_actions = extension['pi-web-agent']['actions']
+            self.system.cmdactions = cmd_actions
+        except:
+            pass
+            
+class System(object):
+    
+    def __init__(self):
+        self.actions=None
+        self.cmdactions=None
+        
+
+    
