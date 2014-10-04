ffeb390179d46adaec2a14b37268949e9be388b3
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-10-04 19:07:39 +0000

    Menu icons

--- usr/libexec/pi-web-agent/css/framework.js
+++ usr/libexec/pi-web-agent/css/framework.js
@@ -14,21 +14,29 @@ function View() {
     self.extensionsOverflow = [];
     self.extensionLimit = 5;
     fetchViewData();
+   
     
     
     function fetchViewData() {
         data = getJSONResponse('/cgi-bin/chrome/view.pwa', null);
         var extensionsCounter = 0;
+        var allowed2Words = false;
+        var allowed = 0;
         $.each(data[0]["pi-web-agent"].system.actions, function (key, action) {
-            if (action.title.split(' ').length >= 3)
+            var words = action.title.split(' ').length;
+            if ( words >= 3 - (allowed2Words ? 1 : 0)) {
                 self.extensionsOverflow.push(action);
+            }
             else if (extensionsCounter < self.extensionLimit) {
                 self.extensions.push(action);
+                if (words >= 2)
+                    allowed++;
+                allowed2Words = allowed >= 2;
                 extensionsCounter++; 
             }
             else
                 self.extensionsOverflow.push(action);
-                    });
+        });
         
         $.each(data[1]["pi-web-agent"].actions, function (key, action) {
             self.actions.push(action);
