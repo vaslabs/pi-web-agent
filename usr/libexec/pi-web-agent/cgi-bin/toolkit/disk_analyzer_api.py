a522a82d5b88758c9aef792149d12b34ba5a118e
Argyris Zardilis <azardilis@gmail.com>
2014-10-17 14:52:45 +0100

    changed dir traversal to recursively calc size and added function to convert to format expected by google charts

--- usr/libexec/pi-web-agent/cgi-bin/toolkit/disk_analyzer_api.py
+++ usr/libexec/pi-web-agent/cgi-bin/toolkit/disk_analyzer_api.py
@@ -1,36 +1,74 @@
-import os
+import json
 
-def walk_dir(root, n_level=3):
-    # Like python os.walk but can specify depth level
-    # possibly useful for disk usage analyser
+class FileSystemObject(object):
+
+    def __init__(self, name, parent, size, children):
+        # name :: str
+        # parent :: str - parent's name not ref to parent
+        # size :: int
+        # children :: List[FileSystemObject]
+        self.name = name
+        self.parent = parent
+        self.size = size
+        self.children = children
+
+        
+class DefaultEncoder(json.JSONEncoder):
+
+    def default(self, o):
+        return o.__dict__
+
+
+def traverse_fs(root, parent="", n_levels=3):
+    # root :: str
+    # parent :: str
+    # n_level :: int
     #
-    # Will have to see the format that either d3.js or Google graphs take
-    #
-    # Note: not thoroughly tested (TDD what?)
-
-    def walk_dir_helper(root, n_level, init_level, sep):
-        names = os.listdir(root)
-        dirs, files = [], []
-
-        for f in names:
-            if os.path.isfile(os.path.join(root, f)):
-                files.append(f)
-            else:
-                dirs.append(f)
-            
-        yield root, dirs, files
-
-        for name in dirs:
-            n_root = os.path.join(root, name)
-            if (n_root.count(sep) - init_level) > n_level: break
-            for x in walk_dir_helper(n_root, n_level, init_level, sep):
-                yield x
-
-    assert(n_level > 0)
-    sep = os.sep
-    root_level = root.count(sep)
+    # for now ignore n_levels
+    from os.path import getsize
+    from os.path import join
     
-    return walk_dir_helper(root, n_level, root_level, sep)
+    names = os.listdir(root)
+    dirs, files = list(), list()
+    
+    for f in names:
+        if os.path.isfile(join(root, f)):
+            files.append(f)
+        else:
+            dirs.append(f)
 
-if __name__ == "__main__":
-    pass
+    children = list(FileSystemObject(f, root, getsize(join(root, f)), list()) for f in files)
+
+    for name in dirs:
+        n_root = join(root, name)
+        children.append(traverse_fs(root=n_root, parent=root))
+    
+    dir_size = sum(f.size for f in children)
+
+    return FileSystemObject(root, parent, dir_size, children)
+
+    
+def get_chart_format(root_dir):
+    # root_dir :: FileSystemObject
+    from collections import deque
+    
+    to_process = deque()
+    entry_list = list()
+    
+    to_process.append(root_dir)
+    while to_process:
+        f = to_process.popleft()
+        entry_list.append([f.name, f.parent, f.size])
+
+        for child in f.children:
+            to_process.append(child)
+
+    return entry_list
+
+    
+def main_test():
+    root_dir = traverse_fs("/Users/argyris/workspace/dir_test/dir1/")
+    entry_list = get_chart_format(root_dir)
+    entry_list_json = DefaultEncoder().encode(entry_list)
+    
+    return entry_list_json
