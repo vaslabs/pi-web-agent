345ac4e7a1b54f77e6d7bd50418f46f30838d53f
Andreas Galazis <agalazis@gmail.com>
2014-10-20 02:06:39 +0300

    added missing imports

--- usr/libexec/pi-web-agent/cgi-bin/toolkit/mplayer_api.py
+++ usr/libexec/pi-web-agent/cgi-bin/toolkit/mplayer_api.py
@@ -1,7 +1,15 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
-
+#!/usr/bin/python
+import cgi
+import cgitb
+import os
+cgitb.enable()
+import sys
+import subprocess
+import json
+import http.client
 __author__="andreas"
 __date__ ="$Sep 14, 2014 9:23:40 PM$"
 
