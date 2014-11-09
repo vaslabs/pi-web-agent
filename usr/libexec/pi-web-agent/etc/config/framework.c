0ee1f430fc9aac65f58dfb34993078a1db9365ee
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-11-09 23:16:07 +0000

    Support two types of outputs

--- usr/libexec/pi-web-agent/etc/config/framework.c
+++ usr/libexec/pi-web-agent/etc/config/framework.c
@@ -49,3 +49,27 @@ void outputTemplate(char *template_name) {
     outputTemplateH(template_name, 1);
 }
 
+void printView(char *template_name) {
+    outputTemplate("_main_part1");
+    outputTemplateH(template_name, 0);
+    outputTemplateH("_main_part2", 0);
+}
+
+void printMainView(char *template_name) {
+    printf("Content-type: application/json\n\n");
+    outputTemplateH(template_name, 0);
+}
+
+void printMe(char *template_name) {
+    char *data = getenv("QUERY_STRING");
+    char l1, l2;
+    if(data == NULL) {
+        printView(template_name);
+    } else {
+        if (strcmp(data, "type=js") == 0)
+            printMainView(template_name); 
+        else {
+            printView(template_name);
+        }  
+    }
+}
