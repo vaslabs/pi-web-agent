93c22cc07ad68bb67532e339bc5e3d9678710070
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-11-09 23:16:34 +0000

    Follow two types of outputs support from framework

--- usr/libexec/pi-web-agent/cgi-bin/toolkit/package_recommendations.c
+++ usr/libexec/pi-web-agent/cgi-bin/toolkit/package_recommendations.c
@@ -1,9 +1,6 @@
 #include "/usr/libexec/pi-web-agent/etc/config/framework.c"
 int main() {
-    outputTemplate("_main_part1");
-    outputTemplateH(
-        "package_recommendations_controller", 
-        0
+    printMe(
+        "package_recommendations_controller"
     );
-    outputTemplateH("_main_part2", 0);
 }
