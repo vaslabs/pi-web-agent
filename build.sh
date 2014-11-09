adcbc4632ad362c18fe0c1263e2860fc054301f9
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-11-09 20:47:57 +0000

    Build script ready

---
+++ build.sh
@@ -0,0 +1,93 @@
+
+working_directory=$(mktemp -d)
+CSS_DIR="$working_directory/usr/libexec/pi-web-agent/css"
+WDIR="usr/libexec/pi-web-agent/css"
+LAST_MAIN="usr/libexec/pi-web-agent/templates/_main_part2.htm"
+FIRST_MAIN="usr/libexec/pi-web-agent/templates/_main_part1.htm"
+TEMPLATE_DIR="$working_directory/usr/libexec/pi-web-agent/templates"
+composeFiles() {
+    
+    JS_FILE="$2.$2"
+    
+    echo "" > $CSS_DIR/$JS_FILE
+    for part in $1; do
+        cat $WDIR/$part >>$CSS_DIR/$JS_FILE
+    done
+
+    md5=$(md5sum $CSS_DIR/$JS_FILE | cut -d ' ' -f 1)
+    mv $CSS_DIR/$JS_FILE $CSS_DIR/${md5}.$2
+    echo ${md5}.$2
+}
+
+appendJSToHTML() {
+    head $LAST_MAIN -n -2 >$working_directory/$LAST_MAIN
+    echo "<script src='/css/$1'></script>" >> $working_directory/$LAST_MAIN
+    tail $LAST_MAIN -n 2 >>$working_directory/$LAST_MAIN
+    sudo cp $working_directory/$LAST_MAIN $LAST_MAIN
+    echo $working_directory/$LAST_MAIN
+    
+}
+
+appendCSSToHTML() {
+    echo "<html><head><link rel='stylesheet' href=/css/$1 type='text/css'>" >$working_directory/$FIRST_MAIN
+    cat $FIRST_MAIN >> $working_directory/$FIRST_MAIN
+    sudo cp $working_directory/$FIRST_MAIN $FIRST_MAIN
+}
+
+minifyCSS() {
+    java -jar yuicompressor-2.4.8.jar --type css $CSS_DIR/$1 > pi-web-agent/$WDIR/$1
+}
+
+minifyJS() {
+    java -jar yuicompressor-2.4.8.jar --type js $CSS_DIR/$1 > pi-web-agent/$WDIR/$1
+}
+
+start_compiling() {
+    for file in $(ls *.c); do
+        filename=$(basename $file '.c')
+        gcc $file -o "$filename.pwa"
+    done
+    rm *.c
+}
+
+compilePWA() {
+    cd usr/libexec/pi-web-agent/cgi-bin/toolkit
+    start_compiling
+    cd -
+    cd usr/libexec/pi-web-agent/cgi-bin/chrome
+    start_compiling
+    cd -
+    #framework.c must be in source form to allow other developers
+    #to include it, no need for compiling it
+}
+
+js_to_combine="jquery-1.10.2.min.js bootstrap.min.js bootswatch.js knockout.js system_scripts.js dependency_manager.js general_purpose_scripts.js framework.js"
+
+css_to_combine="blueprint/screen.css bootstrap.css installUninstallSwitch.css"
+
+
+mkdir -p $CSS_DIR
+
+COMBINED_JS=$(composeFiles "$js_to_combine" "js")
+COMBINED_CSS=$(composeFiles "$css_to_combine" "css")
+
+echo $COMBINED_JS
+echo $COMBINED_CSS
+
+mkdir -p $TEMPLATE_DIR
+
+template=$(appendJSToHTML $COMBINED_JS)
+echo $template
+template=$(appendCSSToHTML $COMBINED_CSS)
+echo $template
+
+cd ~ #place yuicompressor-2.4.8.jar to home
+minifyCSS $COMBINED_CSS
+minifyJS $COMBINED_JS
+cd -
+
+compilePWA
+
+#commit current state
+git add .
+git commit -m "Version build"
