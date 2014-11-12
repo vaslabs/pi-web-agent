1093acb4a5cefbb528b0e92be01da1df6aa51fb0
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-11-12 19:42:31 +0000

    Removed lines that are taken care by the framework

--- usr/libexec/pi-web-agent/cgi-bin/index.c
+++ usr/libexec/pi-web-agent/cgi-bin/index.c
@@ -1,6 +1,4 @@
 #include "/usr/libexec/pi-web-agent/etc/config/framework.c"
 int main() {
-    outputTemplate("_main_part1");
-    outputTemplateH("main", 0);
-    outputTemplateH("_main_part2", 0);
+    printMe("main", 0);
 }
