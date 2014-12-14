053951e3c8b61ffcf8c4562fdb83cfcf15be3c60
Argyris Zardilis <azardilis@gmail.com>
2014-12-14 15:10:47 +0000

    added another dir for c files compilation

--- setup_dev.sh
+++ setup_dev.sh
@@ -49,6 +49,9 @@ compilePWA() {
     cd usr/libexec/pi-web-agent/cgi-bin/chrome
     start_compiling
     cd -
+    cd usr/libexec/pi-web-agent/cgi-bin/
+    start_compiling
+    cd -
     #framework.c must be in source form to allow other developers
     #to include it, no need for compiling it
 }
