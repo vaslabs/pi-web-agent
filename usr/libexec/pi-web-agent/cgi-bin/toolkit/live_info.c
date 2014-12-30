6e76db8537fb560afe301f4efe25c8e98fe487dc
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-12-30 16:14:37 +0000

    Visibility of the structure is a bit confusing. Should check with compiler on next build

--- usr/libexec/pi-web-agent/cgi-bin/toolkit/live_info.c
+++ usr/libexec/pi-web-agent/cgi-bin/toolkit/live_info.c
@@ -18,21 +18,6 @@
 #define TRUE "true"
 #define FALSE "false"
 #define fname_update "/var/log/pi-update/pi_packages_update"
-struct sysinfo {
-               long uptime;             /* Seconds since boot */
-               unsigned long loads[3];  /* 1, 5, and 15 minute load averages */
-               unsigned long totalram;  /* Total usable main memory size */
-               unsigned long freeram;   /* Available memory size */
-               unsigned long sharedram; /* Amount of shared memory */
-               unsigned long bufferram; /* Memory used by buffers */
-               unsigned long totalswap; /* Total swap space size */
-               unsigned long freeswap;  /* swap space still available */
-               unsigned short procs;    /* Number of current processes */
-               unsigned long totalhigh; /* Total high memory size */
-               unsigned long freehigh;  /* Available high memory size */
-               unsigned int mem_unit;   /* Memory unit size in bytes */
-               char _f[20-2*sizeof(long)-sizeof(int)]; /* Padding to 64 bytes */
-           };
 int main(int argc, char *argv[]) {
 
     printf("Content-type: text/json\n\n");
