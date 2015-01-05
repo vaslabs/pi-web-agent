a1caee1a51af8fa736cd3d48d61ee8adf8d77040
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-01-05 13:53:06 +0000

    Release

--- usr/libexec/pi-web-agent/etc/config/pi_web_agent.py
+++ usr/libexec/pi-web-agent/etc/config/pi_web_agent.py
@@ -4,7 +4,7 @@ import json
 
 CONFIG_FILE=os.environ['MY_HOME']+"/etc/config/config.cfg"
 CONFIG_PATH=os.environ['MY_HOME']+"/etc/config"
-VERSION="0.4"
+VERSION="0.4-rc-1"
 class Configuration(object):
     
     def __init__(self):
