53ad72e079e2c98273f76003dc3ad7d4677292a8
agalazis <agalazis@gmail.com>
2015-01-06 18:24:56 +0200

    added started check

--- usr/libexec/pi-web-agent/cgi-bin/toolkit/mplayer_api.py
+++ usr/libexec/pi-web-agent/cgi-bin/toolkit/mplayer_api.py
@@ -88,7 +88,7 @@ class MPlayer:
         '''
         startConsumer=os.environ['MY_HOME'] + '/scripts/websocketdBro/bro -m consumer -c '+ os.environ['ssl_cert']+' -k '+os.environ['ssl_key']+ ' -e mplayer </dev/null >/dev/null 2>&1 &'
         '''start only if consumer started otherwise the state of the appplication would be unknown'''
-        if (execute(startConsumer)[1]==1):
+        if ( execute('pidof websocketd')[1] == 1&&execute(startConsumer)[1]==1):
             return 1
         command=("sh -c '[ -p /tmp/mplayer-control ]" 
                  "|| mkfifo /tmp/mplayer-control;"
