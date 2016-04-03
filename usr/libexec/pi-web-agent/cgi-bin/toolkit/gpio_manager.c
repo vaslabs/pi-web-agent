b6a20f01a8dbd5ec4b5233b69f01a17b753f0431
Vasilis Nicolaou <vaslabsco@gmail.com>
2016-04-03 22:43:56 +0000

    Reverted removed c files

---
+++ usr/libexec/pi-web-agent/cgi-bin/toolkit/gpio_manager.c
@@ -0,0 +1,5 @@
+#include "/usr/libexec/pi-web-agent/etc/config/framework.c"
+
+int main() {
+    printMe("gpio_controller");
+}
---
+++ usr/libexec/pi-web-agent/cgi-bin/toolkit/package_recommendations.c
@@ -0,0 +1,6 @@
+#include "/usr/libexec/pi-web-agent/etc/config/framework.c"
+int main() {
+    printMe(
+        "package_recommendations_controller"
+    );
+}
