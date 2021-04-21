6a7c4959a47ef1756778eb850382110292b865c8
Vasilis Nicolaou <vaslabsco@gmail.com>
2021-04-21 18:32:31 +0100

    Fix some issues

--- debian.sh
+++ debian.sh
@@ -2,8 +2,9 @@ cp .goreleaser.yml target/
 cd target
 unzip piwebagent2.zip
 mv .goreleaser.yml piwebagent2/
+cp -r ../scripts piwebagent2/
 cd piwebagent2
 
-goreleaser --snapshot --skip-publish --rm-dist
+goreleaser --snapshot --skip-publish
 
 cd ../..
--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,3 +1,7 @@
+
+echo "Giving permissions to piwebagent to access its static content"
+chown piwebagent2 /usr/share/piwebagent2/assets
+
 echo "Enabling piwebagent2.service"
 systemctl enable piwebagent2.service
 echo "Starting piwebagent2.service"
