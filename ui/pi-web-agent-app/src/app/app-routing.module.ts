a85b010482ad231de78d02c9b3458c7b130aeba5
Vasilis Nicolaou <vaslabsco@gmail.com>
2021-04-30 12:09:29 +0100

    Add to menu

--- ui/pi-web-agent-app/src/app/app-routing.module.ts
+++ ui/pi-web-agent-app/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LiveInfoComponent } from './live-info/live-info.component';
 import { PoweroffComponent } from './poweroff/poweroff.component';
+import { UpdateManagementComponent } from './update-management/update-management.component';
 
 const routes: Routes = [
   { path: '', component: LiveInfoComponent },
-  { path: 'power-management', component: PoweroffComponent },
+  { path: '#power-management', component: PoweroffComponent },
+  { path: '#updates', component: UpdateManagementComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
